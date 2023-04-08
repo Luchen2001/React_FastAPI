0cfbf2d8e6a6294c3c431b11a95333ddd58ab063
Luchen2001 <luczhou@student.unimelb.edu.au>
2023-04-08 15:31:44 +1000

    Basic streaming with mastodon

---
+++ fastapi-react/backend/main.py
@@ -0,0 +1,65 @@
+import asyncio
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import random
+from mastodon import Mastodon, MastodonNotFoundError, MastodonRatelimitError, StreamListener
+import csv, os, time, json
+
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get('/')
+async def root():
+    return {'example': 'this is an example', 'data': 0}
+
+@app.get('/random')
+async def get_random():
+    rn:int = random.randint(0,100)
+    return {'number': rn, 'limit': 100}
+
+@app.get('/random/{limit}')
+async def get_random(limit:int):
+    rn:int = random.randint(0,limit)
+    return {'number': rn, 'limit': limit}
+
+mastodon = {"username": "", "content":'', "url":""}
+@app.get('/mastodon')
+async def get_mastodon():
+    return mastodon
+
+m = Mastodon(
+        api_base_url=f'https://mastodon.world',
+        access_token=os.environ['MASTODON_ACCESS_TOKEN']
+    )
+
+class Listener(StreamListener):
+    def on_update(self, status):
+        #print(json.dumps(status, indent=2, sort_keys=True, default=str))
+        #print('------------------------------')
+        mastodon["username"] = status["account"]["username"]
+        mastodon["content"] = status["content"]
+        mastodon["url"] = status["url"]
+
+
+async def start_mastodon_stream():
+    while True:
+        try:
+            await m.stream_public(Listener(), run_async=True)
+        except (MastodonNotFoundError, MastodonRatelimitError) as e:
+            print(f"Error: {e}")
+            await asyncio.sleep(60)  # Wait 60 seconds before retrying
+
+@app.on_event("startup")
+async def start_streaming():
+    asyncio.create_task(start_mastodon_stream())
---
+++ fastapi-react/frontend/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function App() {
+  const [text, setText] = useState('')
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8000/mastodon")
+    const data = await response.json()
+    setText(data)
+    console.log(data)
+  }
+  useEffect(() => {
+    fetchData()
+    const timer = setInterval(() => fetchData(), 1000); // Fetch data every 1 seconds
+    return () => clearInterval(timer); // Clean up the interval on component unmount
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>The text retrived from Mastodon through fastAPI</h1>
+      <h3>{text.username}</h3>
+      <div dangerouslySetInnerHTML={{ __html: text.content }} />
+      <a href ={text.url}>{text.url}</a>
+    </div>
+  );
+}
+
+export default App;

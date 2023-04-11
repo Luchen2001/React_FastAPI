7a0350df02d1fe07ce584353521547c648e2c68b
Luchen2001 <110806242+Luchen2001@users.noreply.github.com>
2023-04-11 21:58:17 +1000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Install required packages
+sudo apt update
+sudo apt install nginx -y
+sudo apt-get install curl
+sudo apt install nodejs
+sudo apt install npm
+
+# Set up React app
+cd ~/React_FastAPI/fastapi-react/frontend
+npm install
+npm run build
+
+# Set up PM2
+sudo npm install pm2@latest -g
+pm2 start npm --name "React" -- start
+pm2 startup systemd
+
+# Copy Nginx configuration
+nginx_config_file="/etc/nginx/sites-available/react"
+sudo cp nginx_react_config "$nginx_config_file"
+
+# Create symbolic link for Nginx configuration
+sudo ln -s /etc/nginx/sites-available/react /etc/nginx/sites-enabled/
+
+# Restart Nginx
+sudo systemctl restart nginx
+
+

42bc95c119f902593a00bcfa726afe6a05287b48
Luchen2001 <luczhou@student.unimelb.edu.au>
2023-04-11 19:01:48 +1000

    server

--- fastapi-react/backend/fastapi_server.py
+++ fastapi-react/backend/fastapi_server.py
@@ -4,7 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 origins = [
     "http://localhost:3000",
-    "localhost:3000"
+    "localhost:3000",
+    "http://172.26.132.211"
 ]
 app.add_middleware(
     CORSMiddleware,

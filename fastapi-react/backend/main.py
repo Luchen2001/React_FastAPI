7842b65cc15e3c1d18ba70062b771645e4cf11f3
Luchen2001 <luczhou@student.unimelb.edu.au>
2023-04-11 20:48:14 +1000

    port specified in main.py

--- fastapi-react/backend/main.py
+++ fastapi-react/backend/main.py
@@ -2,7 +2,9 @@ import subprocess
 import sys
 
 def main():
-    fastapi_server = subprocess.Popen(["uvicorn", "fastapi_server:app", "--reload"])
+    #fastapi_server = subprocess.Popen(["uvicorn", "fastapi_server:app", "--reload"])
+    fastapi_server = subprocess.Popen(["uvicorn", "fastapi_server:app", "--host", "0.0.0.0", "--port", "8000", "--reload"])
+
     mastodon_stream = subprocess.Popen([sys.executable, "mastodon_stream.py"])
 
     try:
